3e060c06399265fdfaed1ab43694f15b897eb0ea
Matheus Silva <55966458+directorsilvaa@users.noreply.github.com>
2024-06-06 13:34:53 +0000

    sobre3

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -18,20 +18,20 @@ const services = [
         </svg>`,
     },
     {
-        title: "Social media marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
-        </svg>`,
-    },
-    {
         title: "Desenvolvimento de Software",
         description: "Transforme Suas Ideias em Realidade com Nossa Expertise em Desenvolvimento de Software O desenvolvimento de software personalizado é fundamental para empresas que buscam soluções tecnológicas específicas para atender às suas necessidades exclusivas. Nosso serviço de Desenvolvimento de Software oferece soluções sob medida, desde aplicativos web e móveis até sistemas de gestão empresarial (ERP) e muito mais.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
     {
+        title: "Consultoria Comercial",
+        description: "A consultoria comercial é fundamental para empresas que buscam alcançar seus objetivos de negócios, maximizar sua eficiência e impulsionar seu crescimento. Nosso serviço de Consultoria Comercial oferece soluções personalizadas e estratégicas para ajudar sua empresa a alcançar o sucesso em um mercado competitivo.",
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+        </svg>`,
+    },
+    {
         title: "Email marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+        description: "Alcance Seu Público-Alvo e Conquiste Resultados com Nossa Estratégia de Email Marketing O email marketing continua sendo uma das ferramentas mais poderosas para alcançar e envolver seu público-alvo de maneira direta e personalizada. Nosso serviço de Email Marketing oferece soluções eficazes e personalizadas para ajudar sua empresa a construir relacionamentos duradouros e impulsionar as vendas.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
