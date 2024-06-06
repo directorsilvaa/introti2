0def0f49b09074ebafb72265ec2e3825574805f8
Matheus Silva <55966458+directorsilvaa@users.noreply.github.com>
2024-06-06 13:23:33 +0000

    at

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,7 +1,7 @@
 const services = [
     {
         title: "Marketing de mídia social",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+        description: "Conecte-se, Engaje e Cresça com Nossa Estratégia de Mídia Social No mundo digital de hoje, a presença nas redes sociais é essencial para qualquer empresa que deseja se destacar e crescer. Nosso serviço de Marketing de Mídia Social oferece uma abordagem completa e personalizada para conectar sua marca com seu público-alvo, aumentar o engajamento e impulsionar suas vendas.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
