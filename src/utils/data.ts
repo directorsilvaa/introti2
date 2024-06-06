239c00a8c5c401a2daeb746d75ddd55d4ea61cc1
Matheus Silva <55966458+directorsilvaa@users.noreply.github.com>
2024-06-06 13:31:41 +0000

    sobre01

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -12,8 +12,8 @@ const services = [
         </svg>`,
     },
     {
-        title: "Email marketing",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti qui soluta cupiditate",
+        title: "Automação Comercial",
+        description: "Otimize Seus Processos e Aumente Sua Eficiência com a Automação Comercial A automação comercial é essencial para empresas que buscam simplificar operações, reduzir custos e aumentar a produtividade. Nosso serviço de Automação Comercial oferece soluções avançadas e personalizadas para automatizar tarefas rotineiras e melhorar o desempenho do seu negócio.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
@@ -24,8 +24,8 @@ const services = [
         </svg>`,
     },
     {
-        title: "Amazon affiliate marketing",
-        description: "Sunt, ipsam, necessitatibus sint fugit officia laboriosam minima ab ullam at magni et. Quaerat, sint!",
+        title: "Desenvolvimento de Software",
+        description: "Transforme Suas Ideias em Realidade com Nossa Expertise em Desenvolvimento de Software O desenvolvimento de software personalizado é fundamental para empresas que buscam soluções tecnológicas específicas para atender às suas necessidades exclusivas. Nosso serviço de Desenvolvimento de Software oferece soluções sob medida, desde aplicativos web e móveis até sistemas de gestão empresarial (ERP) e muito mais.",
         icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
         </svg>`,
     },
